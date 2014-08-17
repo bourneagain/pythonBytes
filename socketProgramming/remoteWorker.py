f2ab7fed73798aea611f1ac3dea5cc0c3a39a30f
Shyam Rajendran <samateapple@gmail.com>
2014-08-17 15:01:39 -0500

    MP1 Test

--- socketProgramming/remoteWorker.py
+++ socketProgramming/remoteWorker.py
@@ -7,21 +7,25 @@ Clients can connect to the port to have communication.
 import socket               # Import socket module
 import os
 import subprocess
-
+import sys
 
 s = socket.socket()         # Create a socket object
 #host = socket.gethostname() # Get local machine name
+#hostip=socket.gethostbyaddr(host)
 host = '' 
 
-port = 12345 				# Reserve a port for your service.
+port = int(sys.argv[1])
+#sys.exit()
+	# Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
 s.listen(5)                 # Now wait for mac 5client connection.
-print "SERVER STARTED and listening on " + str(port)
+#print "SERVER STARTED and listening on " + str(hostip) + ":"+str(port)
+print "SERVER STARTED and listening on " 
 
 while True:
     connection, addr = s.accept()     # Establish connection with client.
     #listening started
-    msg="thank you for connection to " + str(host)
+    msg="thank you for connection" 
     connection.send(msg)
     try:
         print 'connection from', addr
--- socketProgramming/workerClient.py
+++ socketProgramming/workerClient.py
@@ -12,12 +12,13 @@ import os
 # Connect the socket to the port where the server is listening
 #server_addressList = [('localhost', 12345)]
 #server_addressList = [ ('173.230.11.208', 12345),('172.16.112.129', 12345)]
-filename,server1,port1,server2,port2,command=sys.argv
+#filename,server1,port1,server2,port2,command=sys.argv
 #server_addressList = [ ('172.16.112.129', 12345),('172.16.112.129', 12346)]
-server_addressList = [ (server1,int(port1) ),(server2,int(port2) )]
-print server_addressList
+#server_addressList = [ (server1,int(port1) ),(server2,int(port2) )]
 #sys.exit()
-#server_addressList = [ ('172.16.112.129', 12345), ('localhost', 12345) ]
+server_addressList = [ ('173.230.11.208', 12345),('172.16.112.130', 12345) ]
+script,command=sys.argv
+print server_addressList
 
 for sa in server_addressList:
     print 'connecting to %s port %s' % sa
