848667297619a56e77d96d6a215850821db2d78a
Shyam Rajendran <samateapple@gmail.com>
2014-11-14 02:29:44 -0600

    infixpostfix convertor

--- infixToPostFix.py
+++ infixToPostFix.py
@@ -1,18 +1,37 @@
 #'*4+23' or '423+*' in prefix and postfix notation respectively.
 
 def topost(infix):
+	precedence={}
+	precedence['+']=1
+	precedence['-']=1
+	precedence['*']=2
+	precedence['/']=2
 	res=[]
 	op=[]	
 	for c in infix:
-		if c.isdigit():
+
+		if c in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" or c.isdigit():
 			res.append(c)
 		else:
-			while len(op) > 0 and op[len(op)-1] in ['*','/'] and c in ['+','-']: 
+			while(len(op)>0 and precedence[c]<=precedence[op[-1]]):
+
+				print "len(op),c,precedence[c],op[-1]"
+				print len(op),c,precedence[c],op[-1]
 				res.append(op.pop())
 			op.append(c)
+	
 	while(len(op)>0):
-		res.append(op.pop())
+		res.append(op.pop())		
 	return res
 
-infix="4+5*2"
+infix="A*B+C*D"
 print topost(infix)
+#
+#[]
+#[]
+#['+']
+#['+']
+#['+', '*']
+#['+', '*']
+#['+', '*', '-']
+#['1', '2', '3', '4', '-', '*', '+']
