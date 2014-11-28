3194a56571c83631e7c2e8a84238bd66c242059b
Shyam Rajendran <samateapple@gmail.com>
2014-11-28 14:36:14 -0600

    gcd using Euclid method

--- gcd.py
+++ gcd.py
@@ -2,5 +2,11 @@ def gcd(x,y):
 	while y!=0:
 		(x,y)=(y,x%y)
 	return x
+def gcd_r(x,y):
+	print x,":",y
+	if y==0:
+		return x
+	else:
+		return gcd_r(y,x%y) 
 
-print gcd(5000,10000000)
+print gcd_r(10,5)
