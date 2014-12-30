726c31bb69f793814d06cff302d2bc05e37308e1
Shyam Rajendran <rshyam.psg@gmail.com>
2014-12-30 11:57:01 -0600

    check permuation.py

--- twosum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    # @return a tuple, (index1, index2)
-    def twoSum(self, num, target):
-		check={}
-		for index, i in enumerate(num):
-			if target - i in check:
-				return check[target-i]+1,index+1
-			check[num[index]]=index
-
