e48c4568542eda6eb155945097e893533300ec8e
Shyam Rajendran <rshyam.psg@gmail.com>
2015-01-01 13:06:37 -0600

    cluster/sort anagrams in list of strings

---
+++ sortAnagram.py
@@ -0,0 +1,33 @@
+
+import sys,pprint
+def a_sort(lst):
+    return sorted(lst, key=lambda elem: (sorted(elem), elem))
+
+def a_cluster(lst):
+	asp = {}
+	for elem in lst:
+		print "for elm",
+		print elem
+		sig = str(sorted(elem))
+		if sig not in asp:
+			asp[sig] = []
+		asp[sig].append(elem)
+	print asp
+	sys.exit(0)
+	result = []
+	for variants in asp.values():
+		result.extend(variants)
+	return result
+
+a=['god', 'dog', 'abc', 'cab', 'man']
+print a_cluster(a)
+
+"""
+
+for elm god
+for elm dog
+for elm abc
+for elm cab
+for elm man
+{"['a', 'm', 'n']": ['man'], "['d', 'g', 'o']": ['god', 'dog'], "['a', 'b', 'c']": ['abc', 'cab']}
+"""
