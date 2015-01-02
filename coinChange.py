b6ac1d9575d9311c7aeab4e7f629f555ee6ffcd7
Shyam Rajendran <rshyam.psg@gmail.com>
2015-01-02 03:43:39 -0600

    coin exchange problem using dynamic programming

--- coinChange.py
+++ coinChange.py
@@ -1,22 +1,13 @@
 def coin_change(coins,sum):
-	 #(1, 3, 6, 7) 
-	 #9
 	dp={}
 	dp[0]=0
-	override=False
 	if sum == 0:
 		return 0
-
 	for j in range(1,sum+1):
-		temp=[]
-		#print j
-		for vi in coins:
-			if vi<=j:
-				temp.append(dp[j-vi])
-		dp[j]=1+min(temp)
+		dp[j]=1+min(dp[j-vi] for vi in coins if vi<=j)
 	print dp[sum]
 
-coin_change([1,3,6,7],5)
+coin_change([1, 5, 10, 21, 25],63)
 			
 
 
