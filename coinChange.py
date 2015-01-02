3bb5db37d61e7d85b4bd7c455a130750324ed4fa
Shyam Rajendran <rshyam.psg@gmail.com>
2015-01-02 03:39:20 -0600

    coin exchange problem using sick dp fuck

---
+++ coinChange.py
@@ -0,0 +1,25 @@
+def coin_change(coins,sum):
+	 #(1, 3, 6, 7) 
+	 #9
+	dp={}
+	dp[0]=0
+	override=False
+	if sum == 0:
+		return 0
+
+	for j in range(1,sum+1):
+		temp=[]
+		#print j
+		for vi in coins:
+			if vi<=j:
+				temp.append(dp[j-vi])
+		dp[j]=1+min(temp)
+	print dp[sum]
+		#print min(dp[j-vi] for vi in coins if vi<=j)
+
+coin_change([1,3,6,7],5)
+			
+
+
+
+
