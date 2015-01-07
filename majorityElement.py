6d6168af16aebddfd8096496dbc651ed17ed1487
Shyam Rajendran <rshyam.psg@gmail.com>
2015-01-07 00:55:41 -0600

    majority element code corrected

--- majorityElement.py
+++ majorityElement.py
@@ -2,7 +2,6 @@ def m(a):
 	c=0
 	m=a[0]
 	for i in a:
-		print i,c,m,"|",
 		if i == m:
 			c+=1
 		else:
@@ -10,8 +9,6 @@ def m(a):
 		if c==0:
 				m=i
 				c=1
-		print i,c,m,"END"
-	print m
 	c=0
 	for i in a:
 		if i == m:
