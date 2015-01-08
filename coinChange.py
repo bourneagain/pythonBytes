535bf14b125210625d9bc179a18816cc391f6cbc
Shyam Rajendran <rshyam.psg@gmail.com>
2015-01-07 21:17:17 -0600

    modified coinchange code with dp

--- coinChange.py
+++ coinChange.py
@@ -1,15 +1,12 @@
-def coin_change(coins,sum):
-	dp={}
-	dp[0]=0
-	if sum == 0:
-		return 0
-	for j in range(1,sum+1):
-		dp[j]=1+min(dp[j-vi] for vi in coins if vi<=j)
-	print dp[sum]
+def dpMakeChange(coinValueList,change):
+	minCoins={}
+	for cents in range(change+1):
+		coinCount = cents
+		for j in [c for c in coinValueList if c <= cents]:
+			if minCoins[cents-j] + 1 < coinCount:
+				coinCount = minCoins[cents-j]+1
+		print minCoins
+		minCoins[cents] = coinCount
+	return minCoins[change]
 
-coin_change([1, 5, 10, 21, 25],63)
-			
-
-
-
-
+print dpMakeChange([1,2,3],4)
