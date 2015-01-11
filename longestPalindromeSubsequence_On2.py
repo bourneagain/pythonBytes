ea13cdd6afe9b277626100c072767a4d5baffc9c
Shyam Rajendran <rshyam.psg@gmail.com>
2015-01-11 11:27:09 -0600

    brute force lps in On2

---
+++ longestPalindromeSubsequence_On2.py
@@ -0,0 +1,27 @@
+def return_all(s):
+	# this is just to get all the possible subsequences of a given string
+	# o(n2) run time
+	end=len(s)
+
+	count=0
+	for start in xrange(0,end):
+		#print "DONE"
+		lend=end
+		while start!=lend:
+			if len(s[start:lend])>1:
+				yield s[start:lend]
+			else:
+				pass
+			count+=1
+			lend-=1
+
+
+def if_palindrome(s):
+	return s == s[::-1]
+
+def lps(s):
+	for sub_sequence in return_all(s):
+		if if_palindrome(sub_sequence):
+		 	return sub_sequence
+
+print lps('dabcbap')
