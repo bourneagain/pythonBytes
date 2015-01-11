3f436d9b65c94b2218c8e9075c3766062918c148
Shyam Rajendran <rshyam.psg@gmail.com>
2015-01-10 19:31:35 -0600

    dp solution to unconcatenating sentence:

--- unconcatenateSentence.py
+++ unconcatenateSentence.py
@@ -1,27 +1,32 @@
 def check_dict(_str):
-	dict_=['is','a','this','man','kumar','i']
-	print _str,":"
+	dict_=['is','a','this','man','kumar','i','was']
 	return _str in dict_
 
 def split_sentence(sentence):
-	dp=[False]*len(sentence)
+	dp=[False]*(len(sentence)+1)
 	#dp[i]: will say that that sentence[0:i] can be split into dictionary words
-	for i in range(1,len(sentence)-1):
-		if dp[i] == False and check_dict(sentence[0:i]):
-			dp[i]=True
+	for i in range(1,len(sentence)):
+		if dp[i] == False and check_dict(sentence[0:i+1]):
+			dp[i+1]=True
 
 		if dp[i] == True:
 			#check for the suffix now if valid with this prefix
-			for j in range(i+1,len(sentence)):
-
+			for j in range(i+1,len(sentence)+1):
 				if dp[j] == False and check_dict(sentence[i:j]):
 					dp[j] = True
-
 				if j == len(sentence) and dp[j] == True:
-					return True
+					pass
 
-	print dp
+	ll = []
+	for i in range(0,len(sentence)+1):
+		if dp[i] == True:
+			ll.append(i)
+	prev=0
+	for i in ll:
+		print sentence[prev:i],
+		prev=i
 	return False
 
-print split_sentence('ithisisa')		
+split_sentence('kumariswasmanthis')		
+
 
