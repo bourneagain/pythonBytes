1a696ad32788c716dd6109730a8f0381fdb263fd
Shyam Rajendran <rshyam.psg@gmail.com>
2015-01-10 23:56:53 -0600

    longest common subsequence in dp

--- lcs_dp.py
+++ lcs_dp.py
@@ -1,11 +1,21 @@
 def lcs(s1, s2):
-	if len(s1)==0 or len(s2)==0: 
-		return 0
-	if s1[0] == s2[0]: 
-		return 1 + lcs(s1[1:], s2[1:])
-	return max(lcs(s1, s2[1:]), lcs(s1[1:], s2))
+	lcs_dp={}
+	l=[]
+	result=0
+	for i in range(len(s1)+1):
+		for j in range(len(s2)+1):
+			if i==0 or j==0:
+				lcs_dp[i,j]=0
+			elif s1[i-1] == s2[j-1]:
+				lcs_dp[i,j]=lcs_dp[i-1,j-1]+1
+				result=max(result,lcs_dp[i,j])
+				l.append(s1[i-1])
+			else:
+				lcs_dp[i,j]=0
 
-s1="AEDFHR"
-s2="ABCDGH"
+	return result,l
+
+s1="sama"
+s2="kumarasamaraj"
 
 print lcs(s1,s2)
