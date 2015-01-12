d47c258a303574d6e3ee9c3bd94e7945409cab34
Shyam Rajendran <rshyam.psg@gmail.com>
2015-01-11 19:10:52 -0600

    added function to return the subarray along with the sum

--- maxSumSubsequence.py
+++ maxSumSubsequence.py
@@ -11,6 +11,28 @@ def maxSumSubsequence(a):
 		#print "RESU",i,"|",	_sum,_max_sum
 	return a,_max_sum
 
+
+def max_sum(a):
+	"""
+	returns start and end position of the array also
+	"""
+	start=0
+	end=0
+	s=0
+	max_s=0
+	for index, i in enumerate(a):
+		s+=i
+		if s < 0:
+			s=0
+			start = index+1
+		elif s > max_s:
+			end=index
+			max_s=s
+	return max_s,(start,end),(a[start:end+1])
+
+a=[-2,1,-3,4,-1,2,1,-5,4]
+print max_sum(a)
+
 A=[5,-9,6,-2,3]
 B=[-2,1,-3,4,-1,2,1,-5,4]
 print maxSumSubsequence(A)
