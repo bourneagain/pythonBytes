c53d0859ac6e050c92b83f70a3afe65fa3b40c66
Shyam Rajendran <rshyam.psg@gmail.com>
2015-01-18 13:25:51 -0600

    move node added

--- linkedList.py
+++ linkedList.py
@@ -79,6 +79,20 @@ class LinkedList:
 		first.next = None
 		head = rest
 
+
+
+
+
+def moveNode(dest, source):
+	# pop one from source and append to head of dest
+	newnode = source
+	source = newnode.next
+	newnode.next = dest
+	dest = newnode
+	return dest,source
+
+
+
 a_list = LinkedList()
 head = Node(1)
 head = a_list.addNode(head, 2)
@@ -86,10 +100,29 @@ head = a_list.addNode(head, 23)
 head = a_list.addNode(head, 90)
 head = a_list.addNode(head, 1)
 head = a_list.addNode(head, 12)
+# a_list.printlist(head)
+# head = a_list.reverseList(head)
+# a_list.printlist(head)
 
+a_list2 = LinkedList()
+head2 = Node(4)
+head2 = a_list.addNode(head2, 5)
+head2 = a_list.addNode(head2, 6)
+
+print "head"
 a_list.printlist(head)
-head = a_list.reverseList(head)
+print "head2"
+a_list2.printlist(head2)
+
+print "----------------DONE----------"
+head,head2=moveNode(head2,head)
+print "head"
 a_list.printlist(head)
+print "head2"
+a_list2.printlist(head2)
+
+
+
 #print a_list.findMth(head, 1)
 # a_list.printlist(head)
 # head = a_list.deleteList(head, 2)
