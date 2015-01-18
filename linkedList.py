c5e197bce14b30d24832f6286a63744a1ebd2042
Shyam Rajendran <rshyam.psg@gmail.com>
2015-01-18 13:13:19 -0600

    added recursive revese

--- linkedList.py
+++ linkedList.py
@@ -13,15 +13,88 @@ class LinkedList:
 		return newNode
 
 	def printlist(self, head):
+		if not head:
+			return None
 		while head.next :
 			print head.data,"->",
 			head = head.next
 		print head.data
+
+	def deleteList(self, head, data):
+		start = head
+		if head.data == data:
+			return head.next
+		while head.next :
+			if head.next.data == data:
+				head.next = head.next.next
+				break
+			else:
+				head = head.next
+		return start
+
+	def removeList(self, head):
+		while head:
+			prev = head
+			head = head.next
+			del prev
+		head = None
+		return head
+
+	def findMth(self, head, m):
+		ref = head
+		while m+1>0:
+			try:
+				head = head.next
+			except AttributeError:
+				return -1
+			m-=1
+
+		while head:
+			head = head.next
+			ref = ref.next
+
+		return ref.data
+	def reverseList(self, head):
+		if not head:
+			return
+		prev = None
+		cur = head
+		while cur:
+			next = cur.next	
+			cur.next = prev
+			prev = cur	
+			cur = next
+		return prev
+
+	def reverseListRecursion(self, head):
+		ref = head
+		if not head:
+			return
+		first = head
+		rest = first.next
+		if not rest:
+			return
+		self.reverseListRecursion(rest)
+		first.next.next = first
+		first.next = None
+		head = rest
 
 a_list = LinkedList()
 head = Node(1)
 head = a_list.addNode(head, 2)
 head = a_list.addNode(head, 23)
 head = a_list.addNode(head, 90)
+head = a_list.addNode(head, 1)
+head = a_list.addNode(head, 12)
 
 a_list.printlist(head)
+head = a_list.reverseList(head)
+a_list.printlist(head)
+#print a_list.findMth(head, 1)
+# a_list.printlist(head)
+# head = a_list.deleteList(head, 2)
+# a_list.printlist(head)
+
+# head = a_list.removeList(head)
+
+
