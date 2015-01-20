6b474f5702607e741c23e26b17875febbe68de9c
Shyam Rajendran <rshyam.psg@gmail.com>
2015-01-19 18:04:12 -0600

    linkedlist copy with random pointer

---
+++ copyListWithRandom.py
@@ -0,0 +1,44 @@
+import collections
+class RandomListNode:
+    def __init__(self, x):
+        self.label = x
+        self.next = None
+        self.random = None
+
+class Solution:
+	def copyRandomList(self, head):
+		ref = head
+		check=collections.defaultdict()
+		check[None] = None
+		if not head:
+			return None
+		while head:
+			check[head] = RandomListNode(head.label)
+			head = head.next
+
+		head = ref
+		while head:
+			check[head].next = check[head.next]
+			head = head.next
+		head = ref
+		while head:
+			check[head].random = check[head.random]
+			head = head.next
+
+		return check[ref]
+
+
+def printlist(head):
+	while head:
+		print head.label
+		head = head.next
+
+n = RandomListNode(1)            
+n2 = RandomListNode(2)
+n.next = n2
+
+
+
+a = Solution()
+b = a.copyRandomList(n)
+printlist(b)
