4a4c4a6df4be3ad00a6718e2ae6a9d8b8b627cfc
Shyam Rajendran <rshyam.psg@gmail.com>
2015-01-19 18:37:15 -0600

    using xor

---
+++ findingNonTwoRepeat.py
@@ -0,0 +1,12 @@
+class Solution:
+    # @param A, a list of integer
+    # @return an integer
+    def singleNumber(self, A):
+        
+        result = 0
+        for i in A:
+        	result^=i
+        return result
+a=Solution()
+n=[1,1,3,3,5,4,4]
+print a.singleNumber(n)
