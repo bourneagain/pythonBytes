9eeed87870ed835660fef2c5826f48e97f6134d9
Shyam Rajendran <rshyam.psg@gmail.com>
2015-01-22 00:51:04 -0600

    merge sort and associated methods

--- linkedList.py
+++ linkedList.py
@@ -71,42 +71,93 @@ class LinkedList:
 		first.next.next = first
 		first.next = None
 		head = rest	
+	
+	def alternateSplitList(self, head):
+		ref = head
+		a= None
+		b= None
+		while ref:
+			a,ref = self.moveNode(a,ref)
+			if ref:
+				b,ref = self.moveNode(b,ref)
+		return a,b
 
-def moveNode(dest, source):
-	# pop one from source and append to head of dest
-	newnode = source
-	source = newnode.next
-	newnode.next = dest
-	dest = newnode
-	return dest,source	
+	def nullHead(self , head):
+		head.next = None
+
+	def merge(self, a , b):
+		toreturn = Node(None)
+		if not a:
+			return b
+		if not b:
+			return a
+		if a.data <= b.data:
+			toreturn = a
+			toreturn.next = self.merge(a.next,b)
+		else:
+			toreturn = b
+			toreturn.next = self.merge(a,b.next)
+		return toreturn
+
+
+	def mergeSort(self, head):
+		if not head or not head.next :
+			return head
+		a,b = self.alternateSplitList(head)
+		print "PRINTING a",self.printlist(a)
+		print "b",self.printlist(b)
+		# a=raw_input('he');
+		l = self.mergeSort(a)
+		r = self.mergeSort(b)
+		merged = self.merge(l,r)
+		return merged
+		# print " asdadasdasdada dhead ", self.printlist(head)
+		
+	def moveNode(self,dest, source):
+		# pop one from source and append to head of dest
+		newnode = source
+		source = newnode.next
+		newnode.next = dest
+		dest = newnode
+		return dest,source	
 
 a_list = LinkedList()
-head = Node(1)
+head = Node(90)
 head = a_list.addNode(head, 2)
-head = a_list.addNode(head, 23)
-head = a_list.addNode(head, 90)
-head = a_list.addNode(head, 1)
 head = a_list.addNode(head, 12)
+head = a_list.addNode(head, 11)
+head = a_list.addNode(head, 43)
+head = a_list.addNode(head, 8)
+a_list.printlist(head)
+# a_list.nullHead(head)
+# a_list.printlist(head)
+# a,b=a_list.alternateSplitList(head)
+# a_list.printlist(a)
+# a_list.printlist(b)
+
+# print "after split"
 # a_list.printlist(head)
 # head = a_list.reverseList(head)
+ref = Node(None)
+head = a_list.mergeSort(head)
+print "here"
+a_list.printlist(head)
+# a_list2 = LinkedList()
+# head2 = Node(4)
+# head2 = a_list.addNode(head2, 5)
+# head2 = a_list.addNode(head2, 6)
+
+# print "head"
 # a_list.printlist(head)
+# print "head2"
+# a_list2.printlist(head2)
 
-a_list2 = LinkedList()
-head2 = Node(4)
-head2 = a_list.addNode(head2, 5)
-head2 = a_list.addNode(head2, 6)
-
-print "head"
-a_list.printlist(head)
-print "head2"
-a_list2.printlist(head2)
-
-print "----------------DONE----------"
-head,head2=moveNode(head2,head)
-print "head"
-a_list.printlist(head)
-print "head2"
-a_list2.printlist(head2)	#print a_list.findMth(head, 1)
+# print "----------------DONE----------"
+# head,head2=moveNode(head2,head)
+# print "head"
+# a_list.printlist(head)
+# print "head2"
+# a_list2.printlist(head2)	#print a_list.findMth(head, 1)
 # a_list.printlist(head)
 # head = a_list.deleteList(head, 2)
 # a_list.printlist(head)
