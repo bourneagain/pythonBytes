ffe43e1d268216a5964040c87c5b33869892732b
Shyam Rajendran <rshyam.psg@gmail.com>
2015-01-30 13:49:20 -0600

    recursive + dp + printing

--- lcs_dp.py
+++ lcs_dp.py
@@ -1,36 +1,38 @@
-def lcs(s1, s2):
-	lcs_dp={}
-	l=[]
-	result=0
-	for i in range(len(s1)+1):
-		for j in range(len(s2)+1):
-			if i==0 or j==0:
-				lcs_dp[i,j]=0
-			elif s1[i-1] == s2[j-1]:
-				lcs_dp[i,j]=lcs_dp[i-1,j-1]+1
-			else:
-				lcs_dp[i,j] = max(lcs_dp[i-1,j], lcs_dp[i,j-1]);
-
-
-	i=len(s1)
-	j=len(s2)
 	
-	res=[0]*lcs_dp[len(s1),len(s2)]
-	l=lcs_dp[len(s1),len(s2)]-1
-	while i>0 and j>0:
-		if s1[i-1] == s2[j-1]:
-			res[l]=s1[i-1]
-			i-=1
-			j-=1
-			l-=1
-		elif lcs_dp[i-1,j] > lcs_dp[i,j-1]:
-			i-=1
-		else:
-			j-=1
+# def lcs(s1, s2):
+# 	lcs_dp={}
+# 	l=[]
+# 	result=0
+# 	for i in range(len(s1)+1):
+# 		for j in range(len(s2)+1):
+# 			if i==0 or j==0:
+# 				lcs_dp[i,j]=0
+# 			elif s1[i-1] == s2[j-1]:
+# 				lcs_dp[i,j]=lcs_dp[i-1,j-1]+1
+# 			else:
+# 				lcs_dp[i,j] = max(lcs_dp[i-1,j], lcs_dp[i,j-1]);
 
-	return lcs_dp[len(s1),len(s2)],''.join(res)
+# 	#below for printing
 
-s1="AGGTAB"
-s2="GXTXAYB"
+# 	i=len(s1)
+# 	j=len(s2)
+	
+# 	res=[0]*lcs_dp[len(s1),len(s2)]
+# 	l=lcs_dp[len(s1),len(s2)]-1
+# 	while i>0 and j>0:
+# 		if s1[i-1] == s2[j-1]:
+# 			res[l]=s1[i-1]
+# 			i-=1
+# 			j-=1
+# 			l-=1
+# 		elif lcs_dp[i-1,j] > lcs_dp[i,j-1]:
+# 			i-=1
+# 		else:
+# 			j-=1
 
-print lcs(s1,s2)
+# 	return lcs_dp[len(s1),len(s2)],''.join(res)
+
+# s1="AGGTAB"
+# s2="GXTXAYB"
+
+# print lcs(s1,s2)
