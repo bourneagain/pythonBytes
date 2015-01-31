1f94e63fc6b3411ad39321d5989ea4a28906eda6
Shyam Rajendran <rshyam.psg@gmail.com>
2015-01-30 20:38:59 -0600

    setPermutation.py

---
+++ setPermutation.py
@@ -0,0 +1,25 @@
+def set_combination(a):
+	n = len(a)
+	bits  = 1<<n
+	res = []
+	for i in range(bits):
+		temp = []
+		one_list = one_index(i)
+		for i in one_list:
+			temp.append(a[i])
+		res.append(temp)
+	return res
+
+def one_index(a):
+	a = int(a)
+	count=[]
+	ci = 0
+	while a > 0:
+		if a&1:
+			count.append(ci)
+		a=a>>1
+		ci+=1
+	return count
+
+print set_combination(('a1','a2','a3'))
+#print one_index(10)
