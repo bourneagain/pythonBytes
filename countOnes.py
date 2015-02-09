3fb1cc7b57b6efa44559b2bd855570611c80ce3d
Shyam Rajendran <rshyam.psg@gmail.com>
2015-02-08 23:40:31 -0600

    very fast with lookup table

--- countOnes.py
+++ countOnes.py
@@ -33,4 +33,44 @@ def countOnes3(n):
     i+=1
   return count
 
-print countOnes2(-1)
+
+# most efficient provided some space is available:
+# precompute
+#http://stackoverflow.com/questions/109023/how-to-count-the-number-of-set-bits-in-a-32-bit-integer
+def fastestCountOnes(n):
+  dp = [ 0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8]
+
+  if n <= 0xff :
+    return dp[n]
+  elif n <= 0xffff:
+    return  dp[n >> 8 ] + dp[n & 0xff]
+  elif n <= 0xffffffff:
+    return  dp[n & 0xff ] + dp[ (n >> 8 ) & 0xff ] + dp[(n >> 16) & 0xff]
+  else:
+    return dp[n & 0xff] + dp[(n >> 8) & 0xff] + dp[(n >> 16) & 0xff] + dp[(n >> 24) & 0xff];
+
+print fastestCountOnes(70000)
+
+
+
+
+
+
+
+
+
